6e6e54878437ed5626839fe59bc17073e424dae7
RosaCielo <ashyzan@gmail.com>
2024-05-30 16:17:30 +0200

    ADD Bonus

---
+++ src/it/gestore/eventi/rosa/MainProgEventi.java
@@ -0,0 +1,13 @@
+package it.gestore.eventi.rosa;
+
+public class MainProgEventi {
+
+	public static void main(String[] args) {
+		ProgrammaEventi test = new ProgrammaEventi("titolo test");
+		
+		test.add();
+		
+
+	}
+
+}
---
+++ src/it/gestore/eventi/rosa/ProgrammaEventi.java
@@ -0,0 +1,32 @@
+package it.gestore.eventi.rosa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammaEventi {
+	
+	private String titoloProgEventi;
+	private List<Evento> Eventi;
+	
+	// costruttore PROGRAMMA EVENTI
+	
+	public ProgrammaEventi(String Titolo) {
+		this.titoloProgEventi = Titolo;
+		List<Evento> NewEventiList = new ArrayList<Evento>();
+		
+		}
+		
+		
+	// METODI DA IMPLEMENTARE
+		
+//	1	un metodo che aggiunge alla lista un Evento, passato come parametro
+//	2	un metodo che restituisce una lista con tutti gli eventi presenti in una certa data
+//	3	un metodo che restituisce quanti eventi sono presenti nel programma
+//	4	un metodo che svuota la lista di eventi
+//	5	un metodo che restituisce una stringa che mostra il titolo del programma e tutti gli eventi ordinati per data nella forma: 
+//		data1 - titolo1
+//		data2 - titolo2 
+		
+		
+
+}
